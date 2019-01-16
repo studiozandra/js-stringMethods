1dddc024786c5ac0b2b60bf7b0b8a36c78326e95
studiozandra <zan@studiozandra.com>
2019-01-15 20:44:05 -1000

    add function howManyLetters

--- exercise.js
+++ exercise.js
@@ -8,6 +8,8 @@
 /*1. Declare a variable named `howManyLetters` and find out how many letters are in the given string below. Console.log your result.*/
 
 var longestPlaceName = "Taumatawhakatangihangakoauauotamateaturipukakapiki- maungahoronukupokaiwhenuakitanatahu";  //Yes, this is a real place located in Porangahau, Central Hawke's Bay.
+var howManyLetters = longestPlaceName.length;
+console.log('how many letter? ' + howManyLetters);
 
 
 
